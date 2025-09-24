4880c6c80be856fcc44436057ac18409b95f6ad8
angeloruiz100 <angeloruiz2200@gmail.com>
2025-09-24 18:46:56 -0500

    promedioNotas.js funcionando con el cambio del if

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,17 @@
+calcularPromedioNotas= function(){
+    let float1=recuperarFlotante("nota1");
+    let float2=recuperarFlotante("nota2");
+    let float3=recuperarFlotante("nota3");
+    let promedio;
+    let resultadoFormateado;
+    promedio=calcularPromedio(float1,float2,float3);
+    resultadoFormateado=promedio.toFixed(2);
+    cambiarTexto("lblResultado",resultadoFormateado);
+    if(resultadoFormateado>8 && resultadoFormateado<=10){
+        cambiarTexto("mensaje","Excelente");
+        cambiarImagen("lblImagen","excellent.gif");
+    }else{
+        cambiarImagen("lblImagen","f.gif");
+        cambiarTexto("mensaje","Reprobado");
+    }
+}
---
+++ promedioNotas/serviciosPromedioNotas.js
@@ -0,0 +1,7 @@
+calcularPromedio= function(n1,n2,n3){
+    let sumaNotas;
+    let promedio;
+    sumaNotas=n1+n2+n3;
+    promedio=sumaNotas/3;
+    return promedio;
+}
